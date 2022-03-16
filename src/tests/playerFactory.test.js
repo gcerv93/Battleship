c737b7b80365bba8c0041b89f54e91a671a9b4d4
gcerv93 <gcerv93@protonmail.com>
2022-03-16 18:43:56 -0500

    Add playerFactory tests

---
+++ src/tests/playerFactory.test.js
@@ -0,0 +1,18 @@
+import playerFactory from "../playerFactory";
+
+test("returns an object with a playerTurn method", () => {
+  let player = playerFactory("player1");
+  expect(player).toHaveProperty("playerTurn");
+});
+
+describe("playerTurn method", () => {
+  test("it sends the receieve attack message to enemyBoard", () => {
+    let player = playerFactory("player1");
+    let coords = [3, 3];
+    let myMock = jest.fn();
+    let enemyBoard = { receiveAttack: myMock };
+    player.playerTurn(coords, enemyBoard);
+    expect(myMock.mock.calls.length).toBe(1);
+    expect(myMock.mock.calls[0][0]).toBe(coords);
+  });
+});
