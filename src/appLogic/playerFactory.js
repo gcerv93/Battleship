2c796a338645cc4d0b03e0748f852b2b2a110571
gcerv93 <gcerv93@protonmail.com>
2022-03-26 12:09:28 -0500

    Remove unused playerFactory module and tests

--- src/appLogic/playerFactory.js
+++
@@ -1,9 +0,0 @@
-const playerFactory = (name) => {
-  function playerTurn(coords, enemyBoard) {
-    enemyBoard.receiveAttack(coords);
-  }
-
-  return { name, playerTurn };
-};
-
-export default playerFactory;
